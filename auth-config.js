17a4f443da37c7c9faf33458bd37f20e94fe7dd7
Kiran <kiran.devkar@flowian.com>
2017-08-09 16:49:01 +0530

    second commit

--- auth-config.js
+++ auth-config.js
@@ -1,11 +1,11 @@
-module.exports = {
-  // App Settings
-  //MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/cuppaOAuth',
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/SocialLoginDB',
-  TOKEN_SECRET: process.env.TOKEN_SECRET || 'doppiaeast',
+// module.exports = {
+//   // App Settings
+//   //MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/cuppaOAuth',
+//   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/SocialLoginDB',
+//   TOKEN_SECRET: process.env.TOKEN_SECRET || 'doppiaeast',
 
-  // OAuth 2.0
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'xxxxxxx',
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'fp91yKvMBfA5ufurefWNNAHB',
-  LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'xxxxxx',
-};
+//   // OAuth 2.0
+//   FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'xxxxxxx',
+//   GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'fp91yKvMBfA5ufurefWNNAHB',
+//   LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'xxxxxx',
+// };
